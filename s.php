cede672cf21677df02af748ad6c2c3e1e8673ba3
mofeedali <38030256+mofeedali@users.noreply.github.com>
2023-03-27 18:07:52 +0300

    Create s.php

---
+++ s.php
@@ -0,0 +1,21 @@
+<?php
+$servername = "csdatabase-instance-1.cpc72fwti9oe.us-east-2.rds.amazonaws.com";
+$database = "CSDATABASE";
+$username = "CSDATABASEadmin";
+$password = "8ivQnwe6Tn6a3KWlCsE4m";
+
+ 
+// Create connection
+ 
+$conn = mysqli_connect($servername, $username, $password, $database);
+ 
+// Check connection
+ 
+if (!$conn) {
+ 
+    die("Connection failed: " . mysqli_connect_error());
+ 
+}
+echo "Connected successfully";
+mysqli_close($conn);
+?>
