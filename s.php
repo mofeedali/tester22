206460d055cdd7881a66d032eb0ade1574df03dc
mofeedali <38030256+mofeedali@users.noreply.github.com>
2023-03-28 01:16:41 +0300

    Update s.php

--- s.php
+++ s.php
@@ -1,13 +1,18 @@
-<?php  
-//Attempt to connect to the default database server
-$link = mysql_connect("csdatabase-instance-1.cpc72fwti9oe.us-east-2.rds.amazonaws.com", "CSDATABASEadmin", "8ivQnwe6Tn6a3KWlCsE4m")
-or die ("Could not connect");  
-//return result pointer containing list of table names
-$list = mysql_list_tables ("CSDATABASE");
-$i = 0;
-//extract table names form the result pointer
-while ($i < mysql_num_rows ($list)) {  
-     $tb_names[$i] = mysql_tablename ($list, $i);
-     echo $tb_names[$i] . "<BR>"; $i++;
+<?php
+$dbname = 'CSDATABASE';
+if (!mysql_connect('csdatabase-instance-1.cpc72fwti9oe.us-east-2.rds.amazonaws.com', 'CSDATABASEadmin', '8ivQnwe6Tn6a3KWlCsE4m')) {
+echo 'Could not connect to mysql';
+exit;
 }
+$sql = "SHOW TABLES FROM $dbname";
+$result = mysql_query($sql);
+if (!$result) {
+echo "DB Error, could not list tables\n";
+echo 'MySQL Error: ' . mysql_error();
+exit;
+}
+while ($row = mysql_fetch_row($result)) {
+echo "Table: {$row[0]}\n";
+}
+mysql_free_result($result);
 ?>
