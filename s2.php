899d0b2bcf2431c3e0ed0a94c5e4ecdd73aa0e1e
mofeedali <38030256+mofeedali@users.noreply.github.com>
2023-04-02 04:46:42 +0300

    Update s2.php

--- s2.php
+++ s2.php
@@ -1,1 +1,37 @@
 
+<?php
+$servername = "csdatabase-instance-1.cpc72fwti9oe.us-east-2.rds.amazonaws.com";
+$username = "CSDATABASEadmin";
+$password = "8ivQnwe6Tn6a3KWlCsE4m";
+$dbname = "CSDATABASE";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+
+
+$sql = "SELECT * FROM users WHERE id=".$_GET['name']."";
+//$sql = "UPDATE users SET role_id=2  WHERE id=9537";
+
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+   // echo "0" . $row[2]. "<br>";
+     $json[] = $row;
+  }
+} else {
+  echo "0 results";
+}
+echo json_encode($json);
+
+
+
+$conn->close();
+
+?> 
